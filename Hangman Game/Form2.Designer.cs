e603bffbc5919adcd98a00d722c9a52377939c52
MarcinLawrenc <marcin.m.lawrenc@gmail.com>
2023-05-23 19:25:02 +0200

    Added Photo

--- Hangman Game/Form2.Designer.cs
+++ Hangman Game/Form2.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.HangmanPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.HangmanPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -63,12 +66,35 @@
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button3.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.button3.Location = new System.Drawing.Point(109, 226);
+            this.button3.Location = new System.Drawing.Point(108, 303);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(160, 80);
             this.button3.TabIndex = 3;
             this.button3.Text = "Play";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(78, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hangman Game";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // HangmanPictureBox
+            // 
+            this.HangmanPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.HangmanPictureBox.Image = global::Hangman_Game.Properties.Resources._8_Stage;
+            this.HangmanPictureBox.Location = new System.Drawing.Point(108, 120);
+            this.HangmanPictureBox.Name = "HangmanPictureBox";
+            this.HangmanPictureBox.Size = new System.Drawing.Size(165, 150);
+            this.HangmanPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.HangmanPictureBox.TabIndex = 33;
+            this.HangmanPictureBox.TabStop = false;
             // 
             // Form2
             // 
@@ -76,12 +102,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(60)))), ((int)(((byte)(68)))));
             this.ClientSize = new System.Drawing.Size(381, 534);
+            this.Controls.Add(this.HangmanPictureBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.HangmanPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -90,5 +120,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox HangmanPictureBox;
     }
 }
--- Hangman Game/Form2.cs
+++ Hangman Game/Form2.cs
@@ -21,5 +21,10 @@ namespace Hangman_Game
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
